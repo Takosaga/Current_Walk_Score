dd703658b9b23bb1306dd20e119096777d768294
Gonzalo Gamez <takosaga@gmail.com>
2022-08-14 17:49:41 +0300

    function of check permissions

--- app/src/main/java/com/example/currentwalkscore/MainActivity.kt
+++ app/src/main/java/com/example/currentwalkscore/MainActivity.kt
@@ -1,11 +1,71 @@
 package com.example.currentwalkscore
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var tvLatitude: TextView
+    private lateinit var tvLongitude: TextView
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        tvLatitude = findViewById(R.id.tv_latitude)
+        tvLongitude = findViewById(R.id.tv_longitude)
+
+        getCurrentLocation()
     }
+
+
+    private fun getCurrentLocation()
+    {
+        if(checkPermissions())
+        {
+            if(isLocationEnabled())
+            {
+
+            }
+            else
+            {
+                //setting open here
+            }
+        }
+        else
+        {
+            //request permission here
+        }
+    }
+
+    companion object{
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION=100
+    }
+
+    private fun checkPermissions(): Boolean
+    {
+        if(ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+            android.Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        return false
+    }
+
+
+
 }
